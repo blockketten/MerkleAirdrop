99bbf4ab787eb56d991a35d30ffddcb9d1dc2792
blockketten <blockketten@gmail.com>
2024-08-07 08:54:14 +0200

    first commit

---
+++ src/GibToken.sol
@@ -0,0 +1,16 @@
+// SDPX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GibToken is ERC20, Ownable {
+    constructor() ERC20("GibToken", "GIB") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
